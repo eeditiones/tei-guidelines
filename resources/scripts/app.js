e9d69106ab408507776324b9083be2505a6e06f5
Wolfgang Meier <wolfgang@existsolutions.com>
2022-03-03 19:20:38 +0100

    Fix element lookup

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -2,7 +2,7 @@ window.addEventListener('WebComponentsReady', function() {
     const pbView = document.getElementById('reference');
     pbEvents.subscribe('pb-autocomplete-selected', 'ident', function(ev) {
         const page = document.querySelector('pb-page');
-        if (pbView && !pbView.getAttribute('static')) {
+        if (pbView && !pbView.hasAttribute('static')) {
             pbEvents.emit('pb-refresh', 'transcription', {
                 xpath: `//(elementSpec|classSpec|macroSpec|attSpec|dataSpec)[@ident='${ev.detail.value}']`
             });
